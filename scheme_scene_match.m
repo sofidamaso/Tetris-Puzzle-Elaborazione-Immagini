94b47c59fb19ebc7e150b75b2dbc61e2c4f37d78
Gioele De Pianto <g.depianto@campus.unimib.it>
2021-02-11 16:50:11 +0100

    Modificato scheme_scene_match

--- scheme_scene_match.m
+++ scheme_scene_match.m
@@ -1,8 +1,21 @@
 %input: immagine schema, immagine scena, (il folder mi serviva per salvare
 %       le foto poi si toglierà)
-%%output: per ora nulla poi metterei la lista dei match
+%output: è una lista di matrici.
+%        per accedere alla matrice della label i si usa output{i}
+%        la matrice è composta da 2 colonne ed un numero di righe uguale ad
+%        i match riscontrati. Prima colonna numero della label della scena
+%        con cui è avvenuto il match. Seconda colonna distanza euclidea 
+%        calcolata. Ad esempio se la label dello schema 2 ha match con la 
+%        label della scena 1 con distanza 0.5 e 3 con distanza 0.2. 
+%        output{2} sarà:
+%        | 3 0.2 |
+%        | 1 0.5 |
+%        i match sono ordinati per distanza ( il primo match è il più
+%        'simile')
 
-function match = scheme_scene_match(scheme_im,scene_im,folder)
+
+function match = scheme_scene_match(scheme_im,scene_im)
+    tic
     %faccio le varie segnmentazione e labelling 
     %tolgo componenti con area < 1000px (da decidere)
     scheme_bw = im2bw(im2gray(scheme_im),graythresh(im2gray(scheme_im)));
@@ -15,46 +28,34 @@ function match = scheme_scene_match(scheme_im,scene_im,folder)
     scheme_signatures = get_signatures(scheme_label);
     scene_signatures = get_signatures(scene_label);
     
-    %scorro tuttel le label di schema e scena
+    
+    
+    %scorro tutte le label di schema e scena
     %per ogni pezzo dello schema tengo come match il pezzo della scena che
     %ha distanza più piccola e < di 1.6
     for i=1 : max(max(scheme_label))
-        dmin = -1;
-        scheme_min = [];
-        scene_min = [];
-        
+        %lista dei match della label i dello schema
+        match_list = [];
         for j = 1 : max(max(scene_label))
             %calcolo distanza
             d = signature_matching(scheme_signatures{i},scene_signatures{j});
-            mask_scheme = scheme_label == i;
-            mask_scene = scene_label == j;
+           
             
             
             if d < 1.6
                 
-                %(qua metteremo il match ora salvo solo le immagini per
-                %stamparle)
-                if dmin == -1
-                    dmin = d;
-                    scheme_min = uint8(mask_scheme).*scheme_im;
-                    scene_min = uint8(mask_scene).*scene_im;
-                else
-                    if d < dmin
-                         dmin = d;
-                        scheme_min = uint8(mask_scheme).*scheme_im;
-                        scene_min = uint8(mask_scene).*scene_im;
-                    end
-                end
+                %nel match oltre ad inserire la label della scena con cui
+                %avviene il match (in questo caso j) inserisco anche la
+                %distanza calcolata (potrebbe tornare utile)
+                match_list = cat(1,match_list , [j d]);
+               
             end
             
         end
-        %se c'è stato un match mi salvo l'immagine
-        if dmin ~= -1
-            figure
-                subplot(1,2,1),imshow(scheme_min),subplot(1,2,2),imshow(scene_min)
-                saveas(gcf,strcat(folder,'/dist',num2str(dmin),'.jpg'));
-                close all;
-        end
+        %riordino i match in base alla distanza 
+        match_list = sortrows(match_list,2);
+        match{i} = match_list;
         
     end
+    toc
 end
