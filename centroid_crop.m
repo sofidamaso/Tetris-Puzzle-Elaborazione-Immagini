af4aaf440afe53c79d208931372f820cb7b77dbb
Sofia <45459355+amarusofia@users.noreply.github.com>
2021-02-15 16:31:37 +0100

    Update centroid_crop.m

--- centroid_crop.m
+++ centroid_crop.m
@@ -16,6 +16,7 @@ function [scene,scene_mask] = centroid_crop(scene,scene_mask,dim)
 c_scene = int32(compute_centroid(scene_mask));
 
 center = int32(ceil(size(scene_mask)/2));
+center = [center(2),center(1)];
 d = center - c_scene;
 scene_mask = imtranslate(scene_mask,d);
 scene = imtranslate(scene,d);
@@ -23,15 +24,20 @@ scene = imtranslate(scene,d);
 dx = floor(dim(2)/2);
 dy = floor(dim(1)/2);
 
-c_scene = int32(compute_centroid(scene_mask));
-
-c_min = c_scene(2) - dx;
-c_max = c_scene(2) + dx;
-r_min = c_scene(1) - dy;
-r_max = c_scene(1) + dy;
+c_min = center(1) - dx;
+c_max = center(1) + dx;
+r_min = center(2) - dy;
+r_max = center(2) + dy;
 
 scene = scene(r_min:r_max-1, c_min:c_max-1, 1:3);
 scene_mask = scene_mask(r_min:r_max-1, c_min:c_max-1);
 
 end
 
+%{
+    PER VISUALIZZARE I PUNTI x E y SULL'IMMAGINE
+  
+    imshow(img)
+    hold on;
+    plot(x, y, 'r+', 'MarkerSize', 15, 'LineWidth', 1);  
+%}
