a54ea2d0fc76cefc84440ac5e6ae5ccf4e95e574
Sofia <45459355+amarusofia@users.noreply.github.com>
2021-02-16 10:53:37 +0100

    Add files via upload

--- script_vecchi/placement.m
+++ script_vecchi/placement.m
@@ -32,37 +32,36 @@
         6. copiamo e incolliamo il tetramino sull'immagine
 %}
 
-function out = placement(scene,scheme,mask_scene,mask_scheme)
+function out = placement(scene,scheme,scene_mask,scheme_mask)
 
 % calcoliamo le aree degli oggetti delle maschere
-a_scene = sum(mask_scene, 'all');
-a_scheme = sum(mask_scheme, 'all');
+a_scene = sum(scene_mask, 'all');
+a_scheme = sum(scheme_mask, 'all');
 
 % ridimensionamento
 scale = a_scheme/a_scene;
-
 scene = imresize(scene,sqrt(scale));
-mask_scene = imresize(mask_scene,sqrt(scale));
+scene_mask = imresize(scene_mask,sqrt(scale));
 
+% controlli ridemensioni e crop
 if scale < 1 % controllo su scena più piccola dello schema 
     
     tmp_scene = zeros(size(scheme));
-    tmp_mask_scene = zeros(size(mask_scheme));
+    tmp_mask_scene = zeros(size(scheme_mask));
 
     tmp_scene(1:size(scene,1),1:size(scene,2),1:size(scene,3)) = scene;
-    tmp_mask_scene(1:size(mask_scene,1),1:size(mask_scene,2)) = mask_scene;
+    tmp_mask_scene(1:size(scene_mask,1),1:size(scene_mask,2)) = scene_mask;
     
     scene = tmp_scene;
-    mask_scene = logical(tmp_mask_scene);
+    scene_mask = logical(tmp_mask_scene);
     
-elseif scale > 1 % sistemare il crop in modo che parta dal centroide
-    mask_scene = mask_scene(1:size(scheme,1), 1:size(scheme,2));
-    scene = scene(1:size(scheme,1), 1:size(scheme,2), 1:3);
+elseif scale > 1   
+    [scene,scene_mask] = centroid_crop(scene,scene_mask,size(scheme_mask));
 end
 
 % calcoliamo gli angoli
-scene_props = bwferet(mask_scene,'MaxFeretProperties');
-scheme_props = bwferet(mask_scheme,'MaxFeretProperties');
+scene_props = bwferet(scene_mask,'MaxFeretProperties');
+scheme_props = bwferet(scheme_mask,'MaxFeretProperties');
 
 scene_angle = scene_props.MaxAngle;
 scheme_angle = scheme_props.MaxAngle;
@@ -70,41 +69,41 @@ scheme_angle = scheme_props.MaxAngle;
 angle = scheme_angle - scene_angle; 
 
 % calcoliamo la rotazione
-c_scheme = int32(compute_centroid(mask_scheme));
+c_scheme = int32(compute_centroid(scheme_mask));
 
-rotations = zeros(4,1);
+rotations = zeros(72,1);
 
-for i = 0:3
+for i = 0:71
     
-    tmp = imrotate(mask_scene,-angle-(90*i),'crop');
+    tmp = imrotate(scene_mask,-angle-(5*i),'crop');
     c_tmp = int32(compute_centroid(tmp));
     d = c_scheme - c_tmp;
     tmp = imtranslate(tmp,d);
 
-    rotations(i+1) = sum(sum(mask_scheme | not(tmp)));
+    rotations(i+1) = sum(sum(scheme_mask | not(tmp)));
 
 end
 
 i = find(rotations == max(rotations))-1;
 
-mask_scene = imrotate(mask_scene,-angle-(90*i),'crop');
-scene = imrotate(scene,-angle-(90*i),'crop');
+scene_mask = imrotate(scene_mask,-angle-(5*i),'crop');
+scene = imrotate(scene,-angle-(5*i),'crop');
 
 % traslazione
-c_scene = int32(compute_centroid(mask_scene));
+c_scene = int32(compute_centroid(scene_mask));
 
 d = c_scheme - c_scene;
-mask_scene = imtranslate(mask_scene,d);
+scene_mask = imtranslate(scene_mask,d);
 scene = imtranslate(scene,d);
 
 % copia e incolla
-red = scene(:,:,1) .* mask_scene; 
-green = scene(:,:,2) .* mask_scene;
-blue = scene(:,:,3) .* mask_scene;
+red = scene(:,:,1) .* scene_mask; 
+green = scene(:,:,2) .* scene_mask;
+blue = scene(:,:,3) .* scene_mask;
 
 tetromino = cat(3,red,green,blue); % singolo tetramino
 
-mask_scene_neg = not(mask_scene); % maschera singolo tetramino - negativo
+mask_scene_neg = not(scene_mask); % maschera singolo tetramino - negativo
 
 red = scheme(:,:,1) .* mask_scene_neg + tetromino(:,:,1);
 green = scheme(:,:,2) .* mask_scene_neg + tetromino(:,:,2);
