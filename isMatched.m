eae16e55ef3460c545cf31518973a58795977ee9
Sofia Damaso <57020694+sofidamaso@users.noreply.github.com>
2021-02-16 19:07:25 +0100

    correzione dimensione output isMatched

--- isMatched.m
+++ isMatched.m
@@ -15,13 +15,10 @@ function out_match = isMatched(match_list,scene_label,scheme_label)
 
 disp=ones(max(max(scheme_label)));
 match=zeros(max(max(scheme_label)),max(max(scene_label)));
-size_list=size(match_list);
 
-for i=1 : size_list(2)   %scorro le matrici delle label
+for i=1 : size(match_list,2)   %scorro le matrici delle label
      m=match_list{i};
-     size_list_i=size(m);
-    for x=1 : size_list_i(1)    %scorro la prima colonna (y=1) della matrice della label i
-     
+    for x=1 : size(match_list{i},1)    %scorro la prima colonna (y=1) della matrice della label i
         if disp(i)==1        %se disponibilità del pezzo è 1 allora posso assegnarlo e poi mettere disponibilità a 0 
             match(i,m(x))=1;
             disp(i)= 0;
@@ -37,13 +34,11 @@ end
 % associato a quello della scena poichè non si hanno 2 pezzi di quel tipo
 % nella scena che possano completare il tetris
 
-size_match=size(match);
-
-for j=1 : size_match(2)
-    for i=1 : size_match(1)
+for j=1 : size(match,2)
+    for i=1 : size(match,1)
         if match(i,j)==1
-            for z=i+1 : size_match(2)
-                   match(z,j)=0;        %
+            for z=i+1 : size(match,1)
+                   match(z,j)=0;        
             end
         end
     end
