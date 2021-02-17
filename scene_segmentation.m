3c047856db811a4b6d700f27fae69cac47a1e190
Gioele De Pianto <g.depianto@campus.unimib.it>
2021-02-17 09:58:46 +0100

    Sostituito seg per distanza euclidea con classificatore

--- scene_segmentation.m
+++ scene_segmentation.m
@@ -14,8 +14,34 @@ cb_mean = 0.425;
 cr_mean = 0.5674;
 
 %mask = threshHCbCrEuclidean(img,h_mean,cb_mean,cr_mean,0.035);
-mask = threshCbCrEuclidean(img,cb_mean,cr_mean,0.035);
+%mask = threshCbCrEuclidean(img,cb_mean,cr_mean,0.035);
 
+R_ycbcr = rgb2ycbcr(im2double(img));
+
+sfondo = im2double(imread('sfondo.png'));
+sfondo_ycbcr = rgb2ycbcr(sfondo);
+
+[r,c,ch] = size(sfondo_ycbcr);
+
+% sfondo_ycbcr è un array con i pixel messi per riga, e tre colonne dei canali
+sfondo_ycbcr = reshape(sfondo_ycbcr,r*c,ch);
+
+% media e deviazione standard dell'immagine sfondo_ycbcr
+m = mean(sfondo_ycbcr);
+v = std(sfondo_ycbcr);
+
+k = 6.5;
+
+[r,c,ch] = size(R_ycbcr);
+image = reshape(R_ycbcr,r*c,ch);
+
+maskcb = image(:,2) >= m(2)-k*v(2) & image(:,2) <= m(2)+k*v(2);
+maskcr = image(:,3) >= m(3)-k*v(3) & image(:,3) <= m(3)+k*v(3);
+
+mask = maskcb & maskcr;
+
+mask = (reshape(mask,r,c,1));
+mask = (mask-1).*-1;
 %serve a "chiudere" le forme che rimangono aperte (es i rettangoli
 %bianchi in alcunu casi)
 mask = imclose(mask,strel('disk',3));
@@ -25,7 +51,7 @@ mask = imfill(mask,'holes');
 
 %erode "aggressiva" che permette di pulire le forme da eventuale
 %bordo rimasto attaccato
-mask = imerode(mask,strel('disk',15));
+mask = imerode(mask,strel('disk',17));
 
 %applico sauovola per eliminare le ombre (le ombre meno scure non
 %vengono eliminate)
