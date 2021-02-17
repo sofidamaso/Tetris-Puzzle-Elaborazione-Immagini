40cd87b5c24fc9930a6975e19e13f8f6637b8248
Sofia <45459355+amarusofia@users.noreply.github.com>
2021-02-17 12:30:20 +0100

    Update scene_segmentation.m

--- scene_segmentation.m
+++ scene_segmentation.m
@@ -1,6 +1,6 @@
 %{
     SCENE_SEGMENTATION
-    Segmenta la scena tramite la media dei canali H, Cb e Cr.
+    Segmenta la scena tramite classificazione.
     
     Input: immagine di scena
     Output: maschera binaria
@@ -8,13 +8,13 @@
 
 function mask = scene_segmentation(img)
     
-%valori calcolati dalle immagini di training per il background
-%h_mean = 0.08825;
-cb_mean = 0.425; 
-cr_mean = 0.5674;
+% valori calcolati dalle immagini di training per il background
+% h_mean = 0.08825;
+% cb_mean = 0.425; 
+% cr_mean = 0.5674;
 
-%mask = threshHCbCrEuclidean(img,h_mean,cb_mean,cr_mean,0.035);
-%mask = threshCbCrEuclidean(img,cb_mean,cr_mean,0.035);
+% mask = threshHCbCrEuclidean(img,h_mean,cb_mean,cr_mean,0.035);
+% mask = threshCbCrEuclidean(img,cb_mean,cr_mean,0.035);
 
 R_ycbcr = rgb2ycbcr(im2double(img));
 
@@ -42,19 +42,20 @@ mask = maskcb & maskcr;
 
 mask = (reshape(mask,r,c,1));
 mask = (mask-1).*-1;
-%serve a "chiudere" le forme che rimangono aperte (es i rettangoli
-%bianchi in alcunu casi)
+
+% serve a "chiudere" le forme che rimangono aperte (es i rettangoli
+% bianchi in alcunu casi)
 mask = imclose(mask,strel('disk',3));
 
-%riempie i buchi generati
+% riempie i buchi generati
 mask = imfill(mask,'holes');
 
-%erode "aggressiva" che permette di pulire le forme da eventuale
-%bordo rimasto attaccato
+% erode "aggressiva" che permette di pulire le forme da eventuale
+% bordo rimasto attaccato
 mask = imerode(mask,strel('disk',17));
 
-%applico sauovola per eliminare le ombre (le ombre meno scure non
-%vengono eliminate)
+% applico sauovola per eliminare le ombre (le ombre meno scure non
+% vengono eliminate)
 hsvim = rgb2hsv(uint8(mask).*img);
 shadow_seg = shadow_mask(hsvim(:,:,3));
 mask = mask.*shadow_seg;
