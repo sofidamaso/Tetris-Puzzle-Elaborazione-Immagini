71616a27b62f751a16e78528d911210fa0a6be71
Sofia <45459355+amarusofia@users.noreply.github.com>
2021-02-18 13:38:13 +0100

    Update scene_segmentation.m

--- scene_segmentation.m
+++ scene_segmentation.m
@@ -6,17 +6,9 @@
     Output: maschera binaria
 %}
 
-function mask = scene_segmentation(img)
-    
-% valori calcolati dalle immagini di training per il background
-% h_mean = 0.08825;
-% cb_mean = 0.425; 
-% cr_mean = 0.5674;
+function mask = scene_segmentation(scene)
 
-% mask = threshHCbCrEuclidean(img,h_mean,cb_mean,cr_mean,0.035);
-% mask = threshCbCrEuclidean(img,cb_mean,cr_mean,0.035);
-
-R_ycbcr = rgb2ycbcr(im2double(img));
+scene_ycbcr = rgb2ycbcr(im2double(scene));
 
 sfondo = im2double(imread('training/sfondo.png'));
 sfondo_ycbcr = rgb2ycbcr(sfondo);
@@ -32,8 +24,8 @@ v = std(sfondo_ycbcr);
 
 k = 6.5;
 
-[r,c,ch] = size(R_ycbcr);
-image = reshape(R_ycbcr,r*c,ch);
+[r,c,ch] = size(scene_ycbcr);
+image = reshape(scene_ycbcr,r*c,ch);
 
 maskcb = image(:,2) >= m(2)-k*v(2) & image(:,2) <= m(2)+k*v(2);
 maskcr = image(:,3) >= m(3)-k*v(3) & image(:,3) <= m(3)+k*v(3);
@@ -41,9 +33,9 @@ maskcr = image(:,3) >= m(3)-k*v(3) & image(:,3) <= m(3)+k*v(3);
 mask = maskcb & maskcr;
 
 mask = (reshape(mask,r,c,1));
-mask = (mask-1).*-1;
+mask = not(mask);
 
-% serve a "chiudere" le forme che rimangono aperte (es i rettangoli
+% chiude le forme che rimangono aperte (es i rettangoli
 % bianchi in alcunu casi)
 mask = imclose(mask,strel('disk',3));
 
@@ -54,13 +46,16 @@ mask = imfill(mask,'holes');
 % bordo rimasto attaccato
 mask = imerode(mask,strel('disk',17));
 
-% applico sauovola per eliminare le ombre (le ombre meno scure non
+% applico sauovola sul canale V per eliminare le ombre (le ombre meno scure non
 % vengono eliminate)
-hsvim = rgb2hsv(uint8(mask).*img);
-shadow_seg = shadow_mask(hsvim(:,:,3));
-mask = mask.*shadow_seg;
+scene_hsv = rgb2hsv(scene);
+scene_v = scene_hsv(:,:,3);
 
-%serve per riempire i buchi creati da sauvola nei pezzi neri
+shadows = shadow_mask(scene_v);
+
+mask = mask & shadows;
+
+% serve per riempire i buchi creati da sauvola nei pezzi neri
 mask = imfill(mask,'holes');
 
 mask = imopen(mask,strel('disk',15));
@@ -68,4 +63,5 @@ mask = imopen(mask,strel('disk',15));
 size_thresh = floor((size(mask,1)*size(mask,2))*0.005);
 
 mask = bwareaopen(mask,size_thresh);
+
 end
