f53b8f99b8db20fce8026bd221d7cd42b6da6d50
Sofia Amarù <45459355+amarusofi@users.noreply.github.com>
2021-02-28 22:07:09 +0100

    Update scheme_scene_match_mindist.m

--- scheme_scene_match_mindist.m
+++ scheme_scene_match_mindist.m
@@ -6,7 +6,7 @@
        
     output{i} lista match del label schema i
     se ad esempio la label schema 2 matcha con label scena 5 e 8
-    output{2} = [ 5 8 ]
+    output{2} = [5 8]
 %}
 
 function match = scheme_scene_match_mindist(training_signatures,scheme_signatures,scene_signatures)
@@ -14,16 +14,16 @@ function match = scheme_scene_match_mindist(training_signatures,scheme_signature
     scheme_class_list = signature_mindist_classifier(training_signatures,scheme_signatures);
     scene_class_list = signature_mindist_classifier(training_signatures,scene_signatures);
   
-    %classe -1 = non trovata
+    % classe -1 = non trovata
     
-    %adesso ho le liste delle classi associate alle varie label
-    %creo quindi la lista di match
+    % adesso ho le liste delle classi associate alle varie label
+    % creo quindi la lista di match
     for i = 1 : size(scheme_class_list,2)
         tmp =[];
         
         for j = 1 : size(scene_class_list,2)
             
-            %se label schema e scena hanno stessa classe inserisco il match
+            % se label schema e scena hanno stessa classe inserisco il match
             if scheme_class_list(i) == scene_class_list(j) && scheme_class_list(i) ~= -1
                 tmp = cat(1,tmp,j);    
             end
