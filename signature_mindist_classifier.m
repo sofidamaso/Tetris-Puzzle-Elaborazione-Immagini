3d45880d26a106231579f9056e00c794b7509db8
Sofia Amarù <45459355+amarusofi@users.noreply.github.com>
2021-02-28 22:09:45 +0100

    Update signature_mindist_classifier.m

--- signature_mindist_classifier.m
+++ signature_mindist_classifier.m
@@ -4,20 +4,20 @@
     	   test_signatures = signatures di tutte le label dell'immagine da classificare
     output: array di match dove match(i) = classe assegnata alla label i dell'immagine
     
-    funzionamento: per ogni label dell'immagine calcolo la distanza  da ogni classe.
+    funzionamento: per ogni label dell'immagine calcolo la distanza da ogni classe.
     	           la distanza tra label e classe è definita nel file signature_matching.
     	           Associo la classe con distanza minima.
 %}
 
 function label_class_list = signature_mindist_classifier(training_signatures,test_signatures)
 
-for i=1 : size(test_signatures,2)
+for i = 1 : size(test_signatures,2)
     distmin = -1;
     class = -1;
 
     for j = 1 : size(training_signatures,2)
 
-        %calcolo distanza
+        % calcolo distanza
         d = signature_matching(test_signatures{i},training_signatures{j});
 
         if d < 2
